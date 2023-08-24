f6f0997e186c70dfed283bab0600d9d846f215d0
TsilaAllaoui <tsila.allaoui@gmail.com>
2023-08-24 16:44:15 +0300

    Added product add page but introduced bug in individual product page

---
+++ src/app/(pages)/add/page.tsx
@@ -0,0 +1,13 @@
+import { CreateForm } from "../../components/CreateForm";
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export default async function AddPage() {
+  return <CreateForm />;
+}
--- src/app/components/CreateForm.tsx
+++ src/app/components/CreateForm.tsx
@@ -1,52 +1,69 @@
 "use client";
 
-import { createProductAction, deleteAllProductsAction } from "../_actions";
 import { useRef, useState } from "react";
 import "../styles/CreateForm.scss";
+import { createProduct, deleteAllProducts } from "../../../db/utilities";
 
 export const CreateForm = () => {
   const [name, setName] = useState("");
   const [price, setPrice] = useState(0);
   const formRef = useRef<HTMLFormElement>(null);
 
-  const submit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    await createProductAction({
-      name: name,
-      price: price,
-      desc: "",
-      images: "",
+  const submit = async (data: FormData) => {
+    const res = await createProduct({
+      name: data.get("name")!.toString(),
+      price: parseFloat(data.get("price")!.toString()),
+      desc: data.get("desc")!.toString(),
+      images: data.get("images")!.toString(),
       createdAt: new Date(),
       updatedAt: new Date(),
       id: "",
-      discount: 0,
-      vendor: null,
+      discount: parseFloat(data.get("discount")!.toString()),
+      vendor: data.get("vendor")!.toString(),
     });
+
     formRef.current!.reset();
   };
 
   const deletAll = (e: React.MouseEvent<HTMLButtonElement>) => {
-    deleteAllProductsAction();
+    deleteAllProducts();
   };
 
   return (
-    <>
-      <form ref={formRef} onSubmit={submit}>
-        <label>Name</label>
-        <input
-          type="text"
-          name="name"
-          onChange={(e) => setName(e.target.value)}
-        />
-        <label>Price</label>
-        <input
-          type="text"
-          name="price"
-          onChange={(e) => setPrice(parseFloat(e.target.value))}
-        />
+    <div id="add">
+      <form ref={formRef} action={submit}>
+        <h1>Add Product</h1>
+        <div id="parts">
+          <div id="left">
+            <label>Name</label>
+            <input
+              type="text"
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+            />
+            <label>Price</label>
+            <input
+              type="text"
+              name="price"
+              onChange={(e) => setPrice(parseFloat(e.target.value))}
+            />
+            <label>Discount</label>
+            <input type="number" max={90} min={10} name="discount" />
+            <label>Vendor</label>
+            <input type="text" name="vendor" />
+          </div>
+          <div id="right">
+            <label>Description</label>
+            <textarea name="desc"></textarea>
+            <label>Images</label>
+            <input type="text" name="images" />
+          </div>
+        </div>
         <button type="submit">Submit</button>
       </form>
-      <button onClick={deletAll}>Erase All</button>
-    </>
+      <button onClick={deletAll} id="submit-button">
+        Erase All
+      </button>
+    </div>
   );
 };
