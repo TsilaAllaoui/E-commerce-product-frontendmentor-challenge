76ec1165b74840e54dbae97fa29cf7247bd2c60c
TsilaAllaoui <tsila.allaoui@gmail.com>
2023-09-02 15:34:46 +0300

    Migrated from sqlite to mongodb

--- prisma/migrations/20230824093422_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- DropIndex
-DROP INDEX "Collection_userId_key";
--- prisma/migrations/20230824093606_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[userId]` on the table `Collection` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Collection_userId_key" ON "Collection"("userId");
